0a949b1800714fd9e1857f2aad30ddd80883a778
AthenaTheRedRose <108251297+AthenaTheRedRose@users.noreply.github.com>
2022-08-20 18:30:30 -0400

    Commit

--- config.lua
+++ config.lua
@@ -2,30 +2,31 @@ Config = {}
 
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
-Config.MaxInventoryWeight = 120000 -- Max weight a player can carry (default 120kg, written in grams)
-Config.MaxInventorySlots = 41 -- Max inventory slots for a player
-
-Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
-Config.MaxDropViewDistance = 12.5 -- The distance in GTA Units that a drop can be seen
-Config.UseItemDrop = false -- This will enable item object to spawn on drops instead of markers
-Config.ItemDropObject = `prop_nigel_bag_pickup` -- if Config.UseItemDrop is true, this will be the prop that spawns for the item
-
-Config.VendingObjects = {
-    "prop_vend_soda_01",
-    "prop_vend_soda_02",
-    "prop_vend_water_01"
-}
-
 Config.BinObjects = {
     "prop_bin_05a",
 }
 
-Config.CraftingObject = `prop_toolchest_05`
+Config.CraftingObject = nil
 
-Config.VendingItem = {
+Config.VendingObjects = {
+    "prop_vend_soda_01",
+    "prop_vend_soda_02",
+    "prop_vend_snak_01",
+    "prop_vend_fridge01",
+    "prop_watercooler",
+    "prop_vend_snak_01_tu",
+    "prop_vend_coffe_01",
+    "prop_vend_water_01",
+    'molo_h_distri',
+    0xA4229512,
+    -1541237486
+    
+}
+
+Config.itemDrink = {
     [1] = {
         name = "kurkakola",
-        price = 4,
+        price = 50,
         amount = 50,
         info = {},
         type = "item",
@@ -33,11 +34,49 @@ Config.VendingItem = {
     },
     [2] = {
         name = "water_bottle",
-        price = 4,
+        price = 50,
         amount = 50,
         info = {},
         type = "item",
         slot = 2,
+    },
+    [3] = {
+        name = "sprunk",
+        price = 50,
+        amount = 5,
+        info = {},
+        type = "item",
+        slot = 3,
+    },
+}
+
+Config.itemSnack = {
+    [1] = {
+        name = "twerks_candy",
+        price = 150,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
+    },
+    [2] = {
+        name = "snikkel_candy",
+        price = 150,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 2,
+    }, -- add the rest of your food here
+}
+
+Config.itemCoffee = {
+    [1] = {
+        name = "coffee",
+        price = 30,
+        amount = 50,
+        info = {},
+        type = "item",
+        slot = 1,
     },
 }
 
@@ -212,7 +251,7 @@ Config.CraftingItems = {
     },
 }
 
-Config.AttachmentCraftingLocation = vector3(88.91, 3743.88, 40.77)
+Config.AttachmentCraftingLocation = nil
 
 Config.AttachmentCrafting = {
     ["items"] = {
@@ -332,6 +371,8 @@ Config.AttachmentCrafting = {
         },
     }
 }
+
+MaxInventorySlots = 40
 
 BackEngineVehicles = {
     [`ninef`] = true,
